03e7f165529614eeba035a9350b1850f50327f54
MagdalenaHo <m.holyszko7@interia.pl>
2020-12-01 20:18:29 +0100

    added zadanie domowe właściwe

--- src/ZadanieDomowe1.java
+++ src/ZadanieDomowe1.java
@@ -1,5 +1,18 @@
 public class ZadanieDomowe1 {
     public static void main(String[] args) {
-        System.out.println("Jestem w zadaniu domowym");
+
+        int firstNumber = 21;
+        int secondNumber =22;
+        int thirdNumber = 15;
+        int forthNumber = 16;
+
+        System.out.println((firstNumber+secondNumber)%2==0);        System.out.println((thirdNumber+forthNumber)%2==0);
+
+        if ((firstNumber+secondNumber)%2==0 && (thirdNumber+forthNumber)%2==0){
+            System.out.println("All numbers are even");
+        }else {
+            System.out.println("All numbers are odd");
+        }
     }
 }
+
